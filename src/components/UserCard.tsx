25c9488fedff925f89c12c9251beb503538016c2
Mishy <chmisheto@gmail.com>
2024-10-31 19:22:52 +0300

    UPDATE

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const UserCard = ({type}: {type:string}) => {
     return (
-        <div className="rounded-2xl odd:bg-pelorous even:bg-elm p-4 flex-1 text-white">
+        <div className="rounded-2xl odd:bg-pelorous even:bg-elm p-4 flex-1 min-w-[130px]">
             <div className="flex justify-between items-center">
-                <span>2024/25</span>
+                <span className="text-[10px] bg-white px-2 py-1 rounded-full text-webOrange">2024/25</span>
                 <Image src="/more.png" alt="" width={20} height={20} />
             </div>
-            <h1>1,045</h1>
-            <h2>{type}</h2>
+            <h1 className="text-2xl font-semibold my-4 text-white">1,045</h1>
+            <h2 className="capitalize font-medium text-white">{type}</h2>
         </div>
     )
 }
